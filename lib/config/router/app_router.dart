d62ae7542d821c76ce700452ebcdbfe2a7bb05c0
Jmendozase6 <jmendozase6@ucvvirtual.edu.pe>
2023-08-07 02:09:01 -0500

    Add config app

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,30 @@
+import 'package:go_router/go_router.dart';
+import 'package:mental_health/presentation/screens/screens.dart';
+import 'package:mental_health/utils/app_router/custom_transition_page.dart';
+import 'package:mental_health/utils/app_router/router_utils.dart';
+
+class AppRouter {
+  static final routes = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: AppRoutes.home.routePath,
+        name: AppRoutes.home.routeName,
+        pageBuilder: (context, state) => buildPageWithDefaultTransition(
+          context: context,
+          state: state,
+          child: const HomeScreen(),
+        ),
+      ),
+      GoRoute(
+        path: AppRoutes.chat.routePath,
+        name: AppRoutes.chat.routeName,
+        pageBuilder: (context, state) => buildPageWithDefaultTransition(
+          context: context,
+          state: state,
+          child: const ChatScreen(),
+        ),
+      ),
+    ],
+  );
+}
---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF407BFF);
+  static const bardMessage = Color(0XFFE6FEFF);
+  static const myMessage = Color(0XFFCCF8F9);
+}
